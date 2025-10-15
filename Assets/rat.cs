9f9724c9293e67a977f530e27314198a18c8ba60
nmuddkidd <Nmuddkidd543@gmail.com>
2025-10-14 20:04:31 -0400

    rat

---
+++ Assets/rat.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class rat : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float Xcoord;
+    private float Ycoord;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
